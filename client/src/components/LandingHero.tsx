1318a6395dbbd14473fccf1acfaf9500d5afcdf8
Flebory <123499686+Flebory@users.noreply.github.com>
2026-05-10 16:19:54 -0400

    Injecting Supabase Env Variables into Deployment

--- client/src/components/LandingHero.tsx
+++ client/src/components/LandingHero.tsx
@@ -3,7 +3,7 @@ const LandingHero = () => {
     return (
         <div className='bg-cream font-serif'>
             <h2 className='text-warm-brown'>Welcome to Forkkast Foods</h2>
-
+            <p className='text-warm-brown text-xl'>This is your personal schedule manager for your meal prepping needs!</p>
         </div>
     )
 }
